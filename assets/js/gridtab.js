7b0538fad4d95f3628a7bfc5cb8c6ceb154333e8
anaiah <carlodominguez@yahoo.com>
2025-04-18 00:13:32 +0800

    apr19 1213a

--- assets/js/gridtab.js
+++ assets/js/gridtab.js
@@ -37,7 +37,7 @@ const  gridtab = {
 }//=======end gridtab obj==========
 
 // Create Tabulator on DOM element with id "table"
-var table = new Tabulator("#claim_grid_update", {
+var table = new Tabulator("#claims_grid_update", {
     ajaxURL: `${myIp}/${util.getCookie('f_region')}/${util.getCookie('grp_id')}/${util.getCookie('f_email')}`, // URL of your API endpoint
     height: "311px", // height of table
     
