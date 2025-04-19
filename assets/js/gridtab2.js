d6e4b3acd32c1758f4a63749d5082a1a506191f6
anaiah <carlodominguez@yahoo.com>
2025-04-19 22:58:18 +0800

    x

--- assets/js/gridtab2.js
+++ assets/js/gridtab2.js
@@ -46,7 +46,7 @@ var pdf_grid = new Tabulator("#pdfprint", {
             headerSort:false, 
             headerHozAlign:"center", 
             resizable:false,
-            width:200,
+            //width:200,
             formatter: function(cell, formatterParams, onRendered){
                 // Access the row data
                 var data = cell.getData();
